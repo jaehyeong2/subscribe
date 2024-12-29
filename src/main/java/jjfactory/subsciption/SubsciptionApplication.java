35ce553024e3ee88ef0c76435f085e5d3e36160a
jaehyeong2 <wogud1514@gmail.com>
2024-12-29 19:40:34 +0900

    init

---
+++ src/main/java/jjfactory/subsciption/SubsciptionApplication.java
@@ -0,0 +1,13 @@
+package jjfactory.subsciption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubsciptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubsciptionApplication.class, args);
+	}
+
+}
